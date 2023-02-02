589e6353c837728ec6027cea00ab4dc3e9f43a57
yujin <miru93198@gmail.com>
2023-02-02 16:10:00 +0900

    marker애 클릭이벤트 오류

--- src/components/Kakao.js
+++ src/components/Kakao.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import dataSet from "./../dataSet.json";
 
 const { kakao } = window;
@@ -6,24 +6,12 @@ const { kakao } = window;
 const Kakao = () => {
   useEffect(() => {
     const container = document.getElementById("map");
+
     const options = {
       center: new kakao.maps.LatLng(37.56682, 126.97865),
       level: 10,
     };
     const map = new kakao.maps.Map(container, options);
-
-    const markerImageUrl = process.env.PUBLIC_URL + `/assets/markerImage.png`,
-      markerImageSize = new kakao.maps.Size(40, 42), // 마커 이미지의 크기
-      markerImageOptions = {
-        offset: new kakao.maps.Point(20, 42), // 마커 좌표에 일치시킬 이미지 안의 좌표
-      };
-
-    // 마커 이미지를 생성한다
-    const markerImage = new kakao.maps.MarkerImage(
-      markerImageUrl,
-      markerImageSize,
-      markerImageOptions
-    );
 
     const state = dataSet.filter((it) => it.BSN_STATE_NM === "영업중"); //영업중인 병원
 
@@ -34,7 +22,21 @@ const Kakao = () => {
           `${it.REFINE_WGS84_LOGT}`
         ), // 마커의 좌표
         map: map,
+        clickable: true, // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
       });
+    });
+
+    const content = '<div className="content">content</div>';
+    const removeable = true;
+
+    const infowindow = new kakao.maps.InfoWindow({
+      content: content,
+      removable: removeable,
+    });
+    // 마커에 클릭이벤트를 등록합니다
+    kakao.maps.event.addListener(marker, "click", function() {
+      // 마커 위에 인포윈도우를 표시합니다
+      infowindow.open(map, marker);
     });
   }, []);
   return (
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,11 +2,39 @@ import React, { useState } from "react";
 import dataSet from "./../dataSet.json";
 
 const Menu = () => {
-  const [data, setData] = useState();
-  //   const info = dataSet.filter((it) => it.SIGUN_NM === "가평군");
-  //   console.log(info);
-  const state = dataSet.filter((it) => it.BSN_STATE_NM === "영업중"); //영업중인 병원
-
+  const menuList = [
+    "수원시",
+    "용인시",
+    "고양시",
+    "성남시",
+    "화성시",
+    "부천시",
+    "남양주시",
+    "안산시",
+    "평택시",
+    "안양시",
+    "시흥시",
+    "김포시",
+    "파주시",
+    "의정부시",
+    "광주시",
+    "광명시",
+    "하남시",
+    "군포시",
+    "오산시",
+    "양주시",
+    "이천시",
+    "구리시",
+    "안성시",
+    "의왕시",
+    "포천시",
+    "양평군",
+    "여주시",
+    "동주천시",
+    "과천시",
+    "가평군",
+    "연천군",
+  ];
   return <div></div>;
 };
 
