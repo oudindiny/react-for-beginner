2ee06ae34cf243066eddb403ec313dbf95218d6b
yujin <miru93198@gmail.com>
2023-02-05 00:42:07 +0900

    kakaomap 오류

--- src/App.js
+++ src/App.js
@@ -10,25 +10,9 @@ import XMLParser from "react-xml-parser";
 import "./App.css";
 
 function App() {
-  const parseStr = (dataSet) => {
-    const dataArr = new XMLParser().parseFromString(dataSet).children;
-    console.log(dataArr);
-  };
-
-  async function getAPI() {
-    await axios({
-      method: "get",
-      url: `https://apis.data.go.kr/B552657/HsptlAsembySearchService/getHsptlMdcncFullDown?serviceKey=xiUcwyft%2BKShrmmbHwBgGx9s1snBLY5ql3SmlbjqVOU5zvnCTo6bPY%2Bk2dTTOH%2BRQrQztGESdXcqF7Y3LjlOPw%3D%3D&pageNo=1&numOfRows=10`,
-    }).then(function(response) {
-      const dataSet = response.data;
-      parseStr(dataSet);
-    });
-  }
-
   return (
     <div>
-      <Form />
-      <Menu />
+      <Home />
       <Router>
         <Routes>
           <Route path="/login" element={<Login />} />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import dataSet from "./../dataSet.json";
+
+const { kakao } = window;
 
 const Home = () => {
-  return <div></div>;
+  useEffect(() => {
+    const container = document.getElementById("map");
+
+    const state = dataSet.filter((it) => it.BSN_STATE_NM === "영업중"); //영업중인 병원
+
+    const main = state.filter((it) => {
+      const lat = `${it.REFINE_WGS84_LAT[0]}`;
+      const logt = `${it.REFINE_WGS84_LOGT[0]}`;
+
+      console.log(lat, logt);
+      const options = {
+        center: new kakao.maps.LatLng(
+          `${it.REFINE_WGS84_LAT[0]}`,
+          `${it.REFINE_WGS84_LOGT[0]}`
+        ),
+        level: 10,
+      };
+      const map = new kakao.maps.Map(container, options);
+
+      const marker = new kakao.maps.Marker({
+        position: new kakao.maps.LatLng(
+          `${it.REFINE_WGS84_LAT[0]}`,
+          `${it.REFINE_WGS84_LOGT[0]}`
+        ), // 마커의 좌표
+        map: map,
+        clickable: true, // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
+      });
+    });
+  });
+  // const options = {
+  //   center: new kakao.maps.LatLng(37.56682, 126.97865),
+  //   level: 10,
+  // };
+  // const map = new kakao.maps.Map(container, options);
+
+  // const marker = state.map((it) => {
+  //   return new kakao.maps.Marker({
+  //     position: new kakao.maps.LatLng(
+  //       `${it.REFINE_WGS84_LAT[0]}`,
+  //       `${it.REFINE_WGS84_LOGT[0]}`
+  //     ), // 마커의 좌표
+  //     map: map,
+  //     clickable: true, // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
+  //   });
+  // });
+
+  return (
+    <div
+      id="map"
+      style={{
+        width: "500px",
+        height: "400px",
+      }}
+    ></div>
+  );
 };
 export default Home;
