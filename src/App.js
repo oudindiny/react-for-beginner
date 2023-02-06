8ca7d104de4352f40182ca84b57c91d6385763cd
yujin <miru93198@gmail.com>
2023-02-06 23:16:14 +0900

    NaverMap 오류

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/Home";
 import Kakao from "./components/Kakao";
 import Form from "./components/Form";
 import Menu from "./components/Menu";
+import NaverMap from "./components/NaverMap";
 import axios from "axios";
 import XMLParser from "react-xml-parser";
 import "./App.css";
@@ -13,6 +14,7 @@ import MainPage from "./pages/MainPage";
 function App() {
   return (
     <div>
+      <NaverMap />
       <Router>
         <Routes>
           <Route path="/login" element={<Login />} />
