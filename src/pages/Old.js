2719996597f1cc7fa2d5ec5317289ae3ea443ce8
yujin <miru93198@gmail.com>
2023-02-08 18:34:41 +0900

    OldPage 제작

--- src/pages/Old.js
+++ src/pages/Old.js
@@ -1,4 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Old = () => {};
+const Old = () => {
+  const [tel, setTel] = useState();
+  const handleTel = (e) => {
+    setTel(e.target.value);
+  };
+  return (
+    <div className="Old">
+      <div className="left">{tel}</div>
+      <div className="right">
+        <button className="tel_number top" value="1">
+          1
+        </button>
+        <button className="tel_number top" value="2">
+          2
+        </button>
+        <button className="tel_number top" value="3">
+          3
+        </button>
+        <button className="tel_number mid" value="4">
+          4
+        </button>
+        <button className="tel_number mid" value="5">
+          5
+        </button>
+        <button className="tel_number mid" value="6">
+          6
+        </button>
+        <button className="tel_number bottom" value="7">
+          7
+        </button>
+        <button className="tel_number bottom" value="8">
+          8
+        </button>
+        <button className="tel_number bottom" value="9">
+          9
+        </button>
+        <button className="tel_number last" value="0">
+          0
+        </button>
+      </div>
+    </div>
+  );
+};
 export default Old;
