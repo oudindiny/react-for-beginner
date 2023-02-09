88c6d5edab5a535cf9144b1c3f0ea9f6f935d9f0
yujin <miru93198@gmail.com>
2023-02-09 17:28:41 +0900

    tel page 제작

--- src/pages/Old.js
+++ src/pages/Old.js
@@ -1,44 +1,94 @@
 import React, { useState } from "react";
 
 const Old = () => {
-  const [tel, setTel] = useState();
-  const handleTel = (e) => {
-    setTel(e.target.value);
-  };
+  const [tel, setTel] = useState("");
+
+  //tel.replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`);
+
   return (
     <div className="Old">
-      <div className="left">{tel}</div>
+      <div className="left">
+        <div className="tel_box">{tel}</div>
+      </div>
       <div className="right">
-        <button className="tel_number top" value="1">
-          1
-        </button>
-        <button className="tel_number top" value="2">
-          2
-        </button>
-        <button className="tel_number top" value="3">
-          3
-        </button>
-        <button className="tel_number mid" value="4">
-          4
-        </button>
-        <button className="tel_number mid" value="5">
-          5
-        </button>
-        <button className="tel_number mid" value="6">
-          6
-        </button>
-        <button className="tel_number bottom" value="7">
-          7
-        </button>
-        <button className="tel_number bottom" value="8">
-          8
-        </button>
-        <button className="tel_number bottom" value="9">
-          9
-        </button>
-        <button className="tel_number last" value="0">
-          0
-        </button>
+        <div>
+          <button
+            className="tel_number"
+            value="1"
+            onClick={() => setTel(tel + "1")}
+          >
+            1
+          </button>
+          <button
+            className="tel_number"
+            value="2"
+            onClick={() => setTel(tel + "2")}
+          >
+            2
+          </button>
+          <button
+            className="tel_number"
+            value="3"
+            onClick={() => setTel(tel + "3")}
+          >
+            3
+          </button>
+        </div>
+        <div>
+          <button
+            className="tel_number"
+            value="4"
+            onClick={() => setTel(tel + "4")}
+          >
+            4
+          </button>
+          <button
+            className="tel_number"
+            value="5"
+            onClick={() => setTel(tel + "5")}
+          >
+            5
+          </button>
+          <button
+            className="tel_number"
+            value="6"
+            onClick={() => setTel(tel + "6")}
+          >
+            6
+          </button>
+        </div>
+        <div>
+          <button
+            className="tel_number"
+            value="7"
+            onClick={() => setTel(tel + "7")}
+          >
+            7
+          </button>
+          <button
+            className="tel_number"
+            value="8"
+            onClick={() => setTel(tel + "8")}
+          >
+            8
+          </button>
+          <button
+            className="tel_number"
+            value="9"
+            onClick={() => setTel(tel + "9")}
+          >
+            9
+          </button>
+        </div>
+        <div>
+          <button
+            className="tel_number"
+            value="0"
+            onClick={() => setTel(tel + "0")}
+          >
+            0
+          </button>
+        </div>
       </div>
     </div>
   );
